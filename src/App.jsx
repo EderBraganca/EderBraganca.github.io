3b9ac6f1f325762cab346e05d4b3304d518321fe
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-07-16 15:39:35 -0300

    Directory files

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { About } from './components/about/about'
+import { Contact } from './components/contact/contact'
+import { Header } from './components/header/header'
+import { Projects } from './components/projects/projects'
+import { Skills } from './components/skills/skills'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </>
   )
 }
