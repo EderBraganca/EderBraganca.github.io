7923d790068f679bc043871c274067f08f6c8a4d
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-07-17 11:52:02 -0300

    skills

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -2,8 +2,17 @@ import './skills.css'
 
 export const Skills = () => {
     return (
-        <section className='skillsSection'>
-            <p>Skills</p>
-        </section>
+        <>
+            <section className='skillsSection'>
+                <p>Major Skills</p>
+                <section className='skillsIcons'>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="React Icon" />
+                    <img src="https://blog.geekhunter.com.br/wp-content/uploads/2020/07/pngwing.com_.png" alt="Java Icon" />
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/027/127/463/small_2x/javascript-logo-javascript-icon-transparent-free-png.png" alt="JS Icon" />
+                    <img src="https://icons.veryicon.com/png/o/business/vscode-program-item-icon/typescript-def.png" alt="TS Icon" />
+                    <img src="https://static-00.iconduck.com/assets.00/spring-icon-2048x2045-yufnoc34.png" alt="Spring Icon" />
+                </section>
+            </section>
+        </>
     )
 }
