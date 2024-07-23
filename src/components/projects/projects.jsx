0ca4ff75716f07db37172c5dd0cf1217588b27cd
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-07-23 12:33:14 -0300

    Images and projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import './projects.css';
 import projects from './projects.json';
 
+const projectsList = projects.projects;
+
 export const Projects = () => {
     return (
-        <>
-            <section className="projectsSection">
-                <p>Projects</p>                
-                {Array.isArray(projects) && projects.map((project, index) => (
-                    <div key={id}>
-                        <h2>{project.name}</h2>
-                        <p>{project.descricao}</p>
+        <section className="projectsSection">
+            <p>Projects</p>
+            <div className="projectsList">
+                {projectsList.map((project) => (
+                    <div key={project.id} className="projectCard">
+                        <a href={project.link} target="_blank" rel="noreferrer">
+                            <section className="textProject">
+                                <h3>{project.name}</h3>
+                                <p>{project.description}</p>
+                            </section>
+                            <img src={project.image} alt={project.name} className='projectImage' />
+                        </a>
                     </div>
                 ))}
-            </section>
-        </>
+            </div>
+        </section>
     )
 }
