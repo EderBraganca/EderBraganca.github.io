d658fbc2df534bdcac1267fee887024866fbf28b
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-07-23 11:24:13 -0300

    Up Projects section

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,9 +1,19 @@
+import React from 'react';
 import './projects.css';
+import projects from './projects.json';
 
 export const Projects = () => {
     return (
-        <section className="projectsSection">
-            <p>Projects</p>
-        </section>
+        <>
+            <section className="projectsSection">
+                <p>Projects</p>                
+                {Array.isArray(projects) && projects.map((project, index) => (
+                    <div key={id}>
+                        <h2>{project.name}</h2>
+                        <p>{project.descricao}</p>
+                    </div>
+                ))}
+            </section>
+        </>
     )
 }
