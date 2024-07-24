377f7a975eb975a12092a570f18ec8f7162b7ad3
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-07-24 13:00:06 -0300

    Contacts

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,17 @@
 import './contact.css';
+import contacts from './contact.json';
 
 export const Contact = () => {
+    const { email, foneNumber, github, linkedin } = contacts.contacts;
+
     return (
-        <section className="contactSection">
-            <p>Contact</p>
+        <section className="contactsSection">
+            <p>Contacts</p>
+
+            <p className='emailLabel'>{email}</p>
+            <p className='foneLabel'>{foneNumber}</p>
+            <a className='githubLink' target='_blank' href={github}>GitHub</a> <br/>         
+            <a className='linkedinLink' target='_blank' href={linkedin}>Linkedin</a>
         </section>
     )
 }
