855506b29bf1a0cbd93a4174697339103dc20eb4
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-07-24 13:00:14 -0300

    Anchor settings

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,16 @@
 import './header.css';
 
 export const Header = () => {
+    const scrollToElement = (elementId) => {
+        const element = document.getElementsByClassName(elementId)[0];
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
     return (
         <header className='menu'>
-            <a href='#'>About</a>
-            <a href='#'>Projects</a>
-            <a href='#'>Skills</a>
-            <a href='#'>Contact</a>
+            <a href='#' onClick={() => scrollToElement('aboutSection')}>About</a>
+            <a href='#' onClick={() => scrollToElement('projectsSection')}>Projects</a>
+            <a href='#' onClick={() => scrollToElement('skillsSection')}>Skills</a>
+            <a href='#' onClick={() => scrollToElement('contactsSection')}>Contact</a>
         </header>
     )
 }
