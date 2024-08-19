c6088ab48b738ff9714859f6b1321ab3fe21cf63
EderBraganca <88894912+EderBraganca@users.noreply.github.com>
2024-08-19 16:44:34 -0300

    Update form

--- src/components/utils/Form.jsx
+++ src/components/utils/Form.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { TextInput, Button } from 'react95';
+import emailjs from '@emailjs/browser';
 
-export const Form = () =>{
+export const Form = () => {
     const [state, setState] = useState({
         name: '',
         email: '',
@@ -9,14 +10,39 @@ export const Form = () =>{
     });
 
     const handleChange = (e) =>
-        setState({ value: e.target.value });
+        setState({ ...state, [e.target.name]: e.target.value });
 
-    const reset = () => setState({ name: '' });
+    const reset = () => setState({ ...state, name: '' });
+    const resetEmail = () => setState({ ...state, email: '' });
+
+    const templateParams = {
+        from_name: state.name,
+        email: state.email,
+        message: state.message,
+    };
+
+    const sendEmail = () => {
+        emailjs.send(
+            'service_peuuo4e',
+            'template_xi7trpq',
+            templateParams,
+            '5aUZolHgfixwTUMZp'
+        )
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Email sent successfully!');
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
+                alert('Failed to send email');
+            });
+    }
 
     return (
-        <div style={{ width: '50vw'}}>
+        <div style={{ width: '50vw' }}>
             <div style={{ display: 'flex' }}>
                 <TextInput
+                    name="name"
                     value={state.name}
                     placeholder='name'
                     onChange={handleChange}
@@ -27,15 +53,30 @@ export const Form = () =>{
                 </Button>
             </div>
             <br />
-            <TextInput
+            <div style={{ display: 'flex' }}>
+                <TextInput
+                    name="email"
                     value={state.email}
                     placeholder='email'
                     onChange={handleChange}
                     fullWidth
                 />
+                <Button onClick={resetEmail} style={{ marginLeft: 4 }}>
+                    Reset
+                </Button>
+            </div>
             <br />
-            <TextInput multiline rows={4} placeholder='Your Text message' fullWidth />
-            
+            <div style={{ display: 'flex' }}>
+                <TextInput
+                    name="message"
+                    multiline
+                    rows={4}
+                    placeholder='Your Text message'
+                    fullWidth
+                    onChange={handleChange}
+                />
+                <Button onClick={sendEmail} style={{ marginLeft: 8 }}> Send</Button>
+            </div>
         </div>
     );
 }
